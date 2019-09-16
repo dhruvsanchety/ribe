84ed57ac1be170257c6497aa63d460aeb822d3c2
Dhruv Sanchety <dsanchety08@gmail.com>
2019-09-16 06:22:38 +0000

    delete

--- app/mailers/message_mailer.rb
+++
@@ -1,8 +0,0 @@
-class MessageMailer < ActionMailer::Base
-  default to: current_user.email
-  def notifcation_email(name, email)
-    @name = name
-    @email = email
-    mail(from: email, subject: 'Message Alert')
-  end
-end
--- app/mailers/request_mailer.rb
+++
@@ -1,8 +0,0 @@
-class RequestMailer < ActionMailer::Base
-  default to: current_user.email
-  def request_email(name, email)
-    @name = name
-    @email = email
-    mail(from: email, subject: 'Request Alert')
-  end
-end
